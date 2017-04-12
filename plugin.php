fe1d8b77e8840dd52d98137ce9a1e3e4a87bbeb6
Freeman <nabil@instafork.com>
2017-04-12 14:55:44 +0100

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -9,28 +9,28 @@
  */
 function wp_api_encode_yoast($data, $post, $context) {
     $yoastMeta = array(
-        'yoast_wpseo_focuskw' => get_post_meta($post['ID'], '_yoast_wpseo_focuskw', true),
-        'yoast_wpseo_title' => get_post_meta($post['ID'], '_yoast_wpseo_title', true),
-        'yoast_wpseo_metadesc' => get_post_meta($post['ID'], '_yoast_wpseo_metadesc', true),
-        'yoast_wpseo_linkdex' => get_post_meta($post['ID'], '_yoast_wpseo_linkdex', true),
-        'yoast_wpseo_metakeywords' => get_post_meta($post['ID'], '_yoast_wpseo_metakeywords', true),
-        'yoast_wpseo_meta-robots-noindex' => get_post_meta($post['ID'], '_yoast_wpseo_meta-robots-noindex', true),
-        'yoast_wpseo_meta-robots-nofollow' => get_post_meta($post['ID'], '_yoast_wpseo_meta-robots-nofollow', true),
-        'yoast_wpseo_meta-robots-adv' => get_post_meta($post['ID'], '_yoast_wpseo_meta-robots-adv', true),
-        'yoast_wpseo_canonical' => get_post_meta($post['ID'], '_yoast_wpseo_canonical', true),
-        'yoast_wpseo_redirect' => get_post_meta($post['ID'], '_yoast_wpseo_redirect', true),
-        'yoast_wpseo_opengraph-title' => get_post_meta($post['ID'], '_yoast_wpseo_opengraph-title', true),
-        'yoast_wpseo_opengraph-description' => get_post_meta($post['ID'], '_yoast_wpseo_opengraph-description', true),
-        'yoast_wpseo_opengraph-image' => get_post_meta($post['ID'], '_yoast_wpseo_opengraph-image', true),
-        'yoast_wpseo_twitter-title' => get_post_meta($post['ID'], '_yoast_wpseo_twitter-title', true),
-        'yoast_wpseo_twitter-description' => get_post_meta($post['ID'], '_yoast_wpseo_twitter-description', true),
-        'yoast_wpseo_twitter-image' => get_post_meta($post['ID'], '_yoast_wpseo_twitter-image', true)
+        'yoast_wpseo_focuskw' => get_post_meta($post->ID, '_yoast_wpseo_focuskw', true),
+        'yoast_wpseo_title' => get_post_meta($post->ID, '_yoast_wpseo_title', true),
+        'yoast_wpseo_metadesc' => get_post_meta($post->ID, '_yoast_wpseo_metadesc', true),
+        'yoast_wpseo_linkdex' => get_post_meta($post->ID, '_yoast_wpseo_linkdex', true),
+        'yoast_wpseo_metakeywords' => get_post_meta($post->ID, '_yoast_wpseo_metakeywords', true),
+        'yoast_wpseo_meta-robots-noindex' => get_post_meta($post->ID, '_yoast_wpseo_meta-robots-noindex', true),
+        'yoast_wpseo_meta-robots-nofollow' => get_post_meta($post->ID, '_yoast_wpseo_meta-robots-nofollow', true),
+        'yoast_wpseo_meta-robots-adv' => get_post_meta($post->ID, '_yoast_wpseo_meta-robots-adv', true),
+        'yoast_wpseo_canonical' => get_post_meta($post->ID, '_yoast_wpseo_canonical', true),
+        'yoast_wpseo_redirect' => get_post_meta($post->ID, '_yoast_wpseo_redirect', true),
+        'yoast_wpseo_opengraph-title' => get_post_meta($post->ID, '_yoast_wpseo_opengraph-title', true),
+        'yoast_wpseo_opengraph-description' => get_post_meta($post->ID, '_yoast_wpseo_opengraph-description', true),
+        'yoast_wpseo_opengraph-image' => get_post_meta($post->ID, '_yoast_wpseo_opengraph-image', true),
+        'yoast_wpseo_twitter-title' => get_post_meta($post->ID, '_yoast_wpseo_twitter-title', true),
+        'yoast_wpseo_twitter-description' => get_post_meta($post->ID, '_yoast_wpseo_twitter-description', true),
+        'yoast_wpseo_twitter-image' => get_post_meta($post->ID, '_yoast_wpseo_twitter-image', true)
     );
 
-    $data['yoast_meta'] = (array) $yoastMeta;
+    $data->data['yoast_meta'] = (array) $yoastMeta;
 
     return $data;
 }
 
-add_filter('json_prepare_post', 'wp_api_encode_yoast', 10, 3);
-add_filter('json_prepare_page', 'wp_api_encode_yoast', 10, 3);
+add_filter('rest_prepare_post', 'wp_api_encode_yoast', 10, 3);
+add_filter('rest_prepare_page', 'wp_api_encode_yoast', 10, 3);
